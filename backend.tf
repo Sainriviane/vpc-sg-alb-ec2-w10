3fdf9ee8effa5e71df82e3d5f8b28359b0735041
Aurore <sainriviane@hotmail.com>
2025-07-09 14:30:13 -0500

    S3 name replacement

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "replace with your bucket name"
+    bucket  = "terraformstate-an"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
